fe8b79299da0776e34cc6106731a46191cb92ad7
adospinoiu <alex.dospinoiu@gmail.com>
2021-04-04 05:42:37 -0700

    Imported and add the MoreVertIcon to Sidebar.js file

--- whatsapp-clone/src/Sidebar.js
+++ whatsapp-clone/src/Sidebar.js
@@ -3,6 +3,7 @@ import './Sidebar.css';
 import IconButton from '@material-ui/core/IconButton';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 
 function Sidebar() {
@@ -18,7 +19,7 @@ function Sidebar() {
                         <ChatIcon />
                     </IconButton>
                     <IconButton>
-                        
+                        <MoreVertIcon />
                     </IconButton>
                 </div>
             </div>
