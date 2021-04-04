44916a2bed0513e4dedf6ec07e08bdb683b5d31d
adospinoiu <alex.dospinoiu@gmail.com>
2021-04-04 05:49:29 -0700

    Imported and added Avator to Sidebar.js file

--- whatsapp-clone/src/Sidebar.js
+++ whatsapp-clone/src/Sidebar.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Sidebar.css';
-import IconButton from '@material-ui/core/IconButton';
+
+import { Avatar, IconButton } from '@material-ui/core';
+
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+
 
 
 function Sidebar() {
@@ -11,6 +15,8 @@ function Sidebar() {
         <div className="sidebar">
             <h5>This is the Sidebar</h5>
             <div className="sidebar__header">
+                <Avatar src='https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2019/07/Man-Silhouette.jpg' />
+
                 <div className="sidebar__headerRight">
                     <IconButton>
                         <DonutLargeIcon />
