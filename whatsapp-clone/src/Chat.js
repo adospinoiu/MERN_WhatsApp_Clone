137b27503d0b48941a010fe7662860c23659869f
adospinoiu <alex.dospinoiu@gmail.com>
2021-04-12 05:30:52 -0700

    Added css to differentiate message and received message to Chat.css

--- whatsapp-clone/src/Chat.js
+++ whatsapp-clone/src/Chat.js
@@ -34,7 +34,7 @@ function Chat() {
                     <span className="chat__timestamp">{new Date().toUTCString()}</span>                   
                 </p>
 
-                <p className="chat__receiver">
+                <p className="chat__message chat__receiver">
                     <span className="chat__name">Alex </span>
                     This is a test message 
                     <span className="chat__timestamp">{new Date().toUTCString()}</span>                   
