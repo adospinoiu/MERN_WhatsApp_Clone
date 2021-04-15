e31fb7537a61240381df5c825703357541affeef
adospinoiu <alex.dospinoiu@gmail.com>
2021-04-15 05:18:15 -0700

    Created GET-api route for getting the data back from the database

--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -26,10 +26,20 @@ app.get('/', (req, res) => {
     res.status(200).send('Hello World')
 });
 
+app.get('/messages/sync', (req, res) => {
+    Messages.find((err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    })
+})
+
 app.post('/messages/new', (req, res) => {
     const dbMessage = req.body
 
-    Messages.create(dbMessage , (err, data) => {
+    Messages.create(dbMessage, (err, data) => {
         if (err) {
             res.status(500).send(err)
         } else {
